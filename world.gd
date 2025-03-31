0f278b017a866fb8d8f7b73c72dab9f7171bbcd8
Nuno Das Neves <ndasneves@gmail.com>
2025-03-30 17:34:49 -0700

    add another song to show song switching

--- world.gd
+++ world.gd
@@ -9,11 +9,13 @@ const confetti_scene = preload("res://vfx/confetti_particles.tscn")
 @onready var _sfx_btn = $HBoxContainer/SFXButton
 @onready var _song_1_btn = $HBoxContainer/Song1Button
 @onready var _song_2_btn = $HBoxContainer/Song2Button
+@onready var _stop_song_btn = $HBoxContainer/StopSongButton
 
 func _ready() -> void:
 	_sfx_btn.connect("button_down", func (): Audio.play_sfx("dungeon_wizard_ding_ding.wav", 0.2))
 	_song_1_btn.connect("button_down", func(): Audio.set_music("dungeon_wizard_menu_music.wav", 0.5))
-	_song_2_btn.connect("button_down", func(): Audio.stop_music())
+	_song_2_btn.connect("button_down", func(): Audio.set_music("sample-3s.mp3", 0.5))
+	_stop_song_btn.connect("button_down", func(): Audio.stop_music())
 	Events.nested_button_pressed.connect(spawn_confetti)
 
 func _process(_delta: float) -> void:
