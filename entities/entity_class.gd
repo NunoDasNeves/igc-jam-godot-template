bdecf9151e65547086d356087c2d7244bf7becc7
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-04-05 18:53:14 -0400

    Addted Direction State
    So far only player direction logic works - working on non player entities logic.

--- entities/entity_class.gd
+++ entities/entity_class.gd
@@ -1,13 +1,25 @@
 class_name Entity extends Node2D
 
-var player_controlled: bool = false
-
+@export var player_controlled: bool = false
+@export_enum("LEFT","RIGHT", "UP", "DOWN") var vision_direction = 2
 var input_dir: Vector2
 var move_dir: Vector2
 
+
+
+
+
 func get_player_input() -> void:
 	var x_in = Input.get_axis("Left", "Right")
+	if x_in == "Left":
+		vision_direction = 0
+	else:
+		vision_direction = 1
 	var y_in = Input.get_axis("Up", "Down")
+	if y_in == "Up":
+		vision_direction = 2
+	else:
+		vision_direction = 3
 	input_dir = Vector2(x_in, y_in).normalized()
 
 func assign_player_is_controlled() -> void:
