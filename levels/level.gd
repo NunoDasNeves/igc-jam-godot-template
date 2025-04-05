b74a95938c59eb03602527bc6b17a4adc17d6333
Nuno Das Neves <ndasneves@gmail.com>
2025-04-05 15:00:10 -0700

    hero and mimic spawn from the stairwells now, respawn when freed

--- levels/level.gd
+++ levels/level.gd
@@ -5,18 +5,31 @@ extends Node
 @onready var other_tiles: TileMapLayer = $OtherTiles
 @onready var wall_tiles: TileMapLayer = $WallTiles
 
-var stairs_up_coords: Array[Vector2i] = []
-var stairs_down_coords: Array[Vector2i] = []
+@export var max_heroes: int = 1
+@export var max_monsters: int = 1
+
+class Spawner extends RefCounted:
+	var coord: Vector2i
+	var timer: SceneTreeTimer
+
+var hero_spawners: Array[Spawner] = []
+var monster_spawners: Array[Spawner] = []
 
 func _ready() -> void:
 	# get the spawner tiles
 	var coords = other_tiles.get_used_cells()
 	for coord: Vector2i in coords:
 		var tile_data: TileData = other_tiles.get_cell_tile_data(coord)
-		if tile_data.get_custom_data("stairs_up"):
-			stairs_up_coords.append(coord)
-		if tile_data.get_custom_data("stairs_down"):
-			stairs_down_coords.append(coord)
+		var spawns_heroes = tile_data.get_custom_data("stairs_up")
+		var spawns_monsters = tile_data.get_custom_data("stairs_down")
+		if spawns_heroes or spawns_monsters:
+			var spawner = Spawner.new()
+			spawner.coord = coord
+			spawner.timer = get_tree().create_timer(0.5)
+			if spawns_heroes:
+				hero_spawners.append(spawner)
+			if spawns_monsters:
+				monster_spawners.append(spawner)
 
 func coord_is_wall(coord: Vector2i) -> bool:
 	var tile_data: TileData = wall_tiles.get_cell_tile_data(coord)
--- world.gd
+++ world.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var entities_container: Node2D = $Entities
 @onready var level: Level = $Level
 
 func _ready() -> void:
@@ -64,6 +65,41 @@ func process_entity(entity: Entity) -> void:
 		if to_center.dot(move_dir) > 0 or !level.coord_is_wall(coord + move_n_dir):
 			entity.move_dir = move_dir
 
+func spawn_entity(spawner: Level.Spawner, scene: PackedScene) -> void:
+	var node = scene.instantiate()
+	entities_container.add_child(node)
+	var pos = level.other_tiles.map_to_local(spawner.coord)
+	node.global_position = pos
+	# this shoould be the case? $Level, $Entities, $World shouldn't be transformed..
+	assert(node.position == node.global_position)
+	# wait a bit before this spawner can be used again
+	spawner.timer = get_tree().create_timer(0.5)
+
+func spawn_entities(spawners: Array[Level.Spawner], num: int, scene:PackedScene) -> void:
+	spawners.shuffle()
+	while num > 0 and spawners.size() > 0:
+		var spawner = spawners.pop_back()
+		if spawner.timer.time_left <= 0:
+			spawn_entity(spawner, scene)
+			num -= 1
+
 func _physics_process(delta: float) -> void:
-	for entity: Entity in $Entities.get_children():
+	for entity: Entity in entities_container.get_children():
 		process_entity(entity)
+
+	# figure out how much stuff to spawn and get the spawners
+	var num_heroes = get_tree().get_node_count_in_group("hero")
+	var num_heroes_to_spawn = max(level.max_heroes - num_heroes, 0)
+	if num_heroes_to_spawn > 0:
+		spawn_entities(level.hero_spawners.duplicate(), num_heroes_to_spawn,  preload("res://entities/hero/hero.tscn"))
+
+	var num_monsters = get_tree().get_node_count_in_group("monster")
+	var num_monsters_to_spawn = max(level.max_monsters - num_monsters, 0)
+	if num_monsters_to_spawn > 0:
+		spawn_entities(level.monster_spawners.duplicate(), num_monsters_to_spawn,  preload("res://entities/mimic/mimic.tscn"))
+
+	# kill first monster in group, for testing spawning
+	if Input.is_key_pressed(KEY_K):
+		var monsters = get_tree().get_nodes_in_group("monster")
+		if monsters.size() > 0:
+			monsters[0].queue_free()
