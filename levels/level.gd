3f8a0f42d8b4cccdf9f4c26fc83b6c8a45e2f8e9
Nuno Das Neves <ndasneves@gmail.com>
2025-04-06 10:38:31 -0700

    add SpawnPoint scene to handle spawning in a more idiomatic way..

--- levels/level.gd
+++ levels/level.gd
@@ -8,28 +8,26 @@ extends Node
 @export var max_heroes: int = 1
 @export var max_monsters: int = 1
 
-class Spawner extends RefCounted:
-	var coord: Vector2i
-	var timer: SceneTreeTimer
+var hero_spawn_points: Array[SpawnPoint] = []
+var monster_spawn_points: Array[SpawnPoint] = []
 
-var hero_spawners: Array[Spawner] = []
-var monster_spawners: Array[Spawner] = []
+func create_spawn_point(coord: Vector2i, time_secs: float) -> SpawnPoint:
+	var point = preload("res://levels/spawn_point.tscn").instantiate()
+	add_child(point)
+	point.coord = coord
+	point.time_secs = time_secs
+	point.position = other_tiles.map_to_local(coord)
+	return point
 
 func _ready() -> void:
 	# get the spawner tiles
 	var coords = other_tiles.get_used_cells()
 	for coord: Vector2i in coords:
 		var tile_data: TileData = other_tiles.get_cell_tile_data(coord)
-		var spawns_heroes = tile_data.get_custom_data("stairs_up")
-		var spawns_monsters = tile_data.get_custom_data("stairs_down")
-		if spawns_heroes or spawns_monsters:
-			var spawner = Spawner.new()
-			spawner.coord = coord
-			spawner.timer = get_tree().create_timer(0.5)
-			if spawns_heroes:
-				hero_spawners.append(spawner)
-			if spawns_monsters:
-				monster_spawners.append(spawner)
+		if tile_data.get_custom_data("stairs_up"):
+			hero_spawn_points.append(create_spawn_point(coord, 0.5))
+		if tile_data.get_custom_data("stairs_down"):
+			monster_spawn_points.append(create_spawn_point(coord, 0.5))
 
 func coord_is_wall(coord: Vector2i) -> bool:
 	var tile_data: TileData = wall_tiles.get_cell_tile_data(coord)
