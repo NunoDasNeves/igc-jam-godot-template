cf3c8dd5c9eb5d578e6c320a5f517bcc742ae03d
Nuno Das Neves <ndasneves@gmail.com>
2025-04-07 16:44:38 -0700

    hero goes toward nearest chest

--- entities/hero/hero_class.gd
+++ entities/hero/hero_class.gd
@@ -7,8 +7,6 @@ extends Entity
 @export_enum("Idle", "Exploring", "Chasing", "Action")
 var hero_state: int
 
-
-
 func _ready() -> void:
 	pass
 
@@ -31,16 +29,40 @@ func _process(delta: float) -> void:
 			#   else move closer
 			pass
 
+# TODO move to Util.gd or similar
+func get_nearest_node2d(gpos: Vector2, nodes: Array[Node2D]) -> Node2D:
+	if nodes.size() == 0:
+		return null
+	var min_node: Node2D = nodes[0]
+	var min_dist_sqrd: float = INF
+
+	for node: Node2D in nodes:
+		var dist_srqd = gpos.distance_squared_to(node.global_position)
+		if dist_srqd < min_dist_sqrd:
+			min_dist_sqrd = dist_srqd
+			min_node = node
+
+	return min_node
+
 func _physics_process(delta: float) -> void:
-	nav_agent.target_position = get_global_mouse_position()
-	var next_pos = nav_agent.get_next_path_position()
-	input_dir = (next_pos - global_position).normalized()
+	if nav_agent.is_navigation_finished():
+		var chests: Array[Node2D]
+		# argh gdscript
+		chests.assign(get_tree().get_nodes_in_group("chest"))
+		var nearest_chest = get_nearest_node2d(global_position, chests)
+		if nearest_chest and nearest_chest is Entity:
+			nav_agent.target_position = nearest_chest.position
+
+	if !nav_agent.is_navigation_finished():
+		var next_pos = nav_agent.get_next_path_position()
+		input_dir = (next_pos - global_position).normalized()
+
 	super(delta)
 
 func hero_looking() -> Entity:
 	# RayCast2D will detect any collider it sees in its path
 	var entity_seen = vision.get_collider()
-	
+
 	if entity_seen == null:
 		print("Hero sees nothing.")
 		return null
