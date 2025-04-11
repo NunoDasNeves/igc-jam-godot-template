81c30616b8f5d2df01ae767d0da128203440c366
Nuno Das Neves <ndasneves@gmail.com>
2025-04-11 09:24:52 -0700

    add char_killed signal and fix respawning of player, demon, hero

--- levels/level.gd
+++ levels/level.gd
@@ -8,15 +8,16 @@ extends Node
 @export var max_heroes: int = 1
 @export var max_monsters: int = 1
 
+var spawn_points: Dictionary[String, Array] = {}
+
 var hero_spawn_points: Array[SpawnPoint] = []
 var monster_spawn_points: Array[SpawnPoint] = []
 var chest_spawn_points: Array[SpawnPoint] = []
 var sight_orb_spawn_points: Array[SpawnPoint] = []
 
-func create_spawn_point(coord: Vector2i, cooldown_secs: float = 0) -> SpawnPoint:
+func create_spawn_point(coord: Vector2i) -> SpawnPoint:
 	var point = preload("res://levels/spawn_point.tscn").instantiate()
 	point.coord = coord
-	point.cooldown_secs = cooldown_secs
 	point.position = other_tiles.map_to_local(coord)
 	add_child(point)
 	return point
@@ -27,13 +28,18 @@ func _ready() -> void:
 	for coord: Vector2i in other_coords:
 		var tile_data: TileData = other_tiles.get_cell_tile_data(coord)
 		if tile_data.get_custom_data("stairs_up"):
-			hero_spawn_points.append(create_spawn_point(coord, 0.5))
+			hero_spawn_points.append(create_spawn_point(coord))
 		if tile_data.get_custom_data("stairs_down"):
-			monster_spawn_points.append(create_spawn_point(coord, 0.5))
+			monster_spawn_points.append(create_spawn_point(coord))
 		if tile_data.get_custom_data("chest"):
 			chest_spawn_points.append(create_spawn_point(coord))
 		if tile_data.get_custom_data("sight_orb"):
 			sight_orb_spawn_points.append(create_spawn_point(coord))
+
+	spawn_points["hero"] = hero_spawn_points
+	spawn_points["monster"] = monster_spawn_points
+	spawn_points["chest"] = chest_spawn_points
+	spawn_points["sight_orb"] = sight_orb_spawn_points
 
 	# create pathing tiles
 	var floor_coords = floor_tiles.get_used_cells()
