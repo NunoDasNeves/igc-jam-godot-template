acbc4b1058aacb99c4c170948e9f6904c80623db
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-04-19 14:59:32 -0400

    e
    e

--- ui/hunger_meter.gd
+++ ui/hunger_meter.gd
@@ -5,3 +5,11 @@ signal hunger_meter_empty
 func _on_value_changed(value: float) -> void:
 	if value == 0:
 		emit_signal("hunger meter empty")
+
+
+# (in hunger_meter.gd)
+func _ready():
+	Events.hero_eaten.connect(ate_hero)
+
+func ate_hero():
+	value -= 20
