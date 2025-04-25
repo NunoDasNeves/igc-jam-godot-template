0b1dc4c6dc9ad4abf5eb41fffde8782cfdd422de
Nuno Das Neves <ndasneves@gmail.com>
2025-04-25 10:51:22 -0700

    use dungeon_ambience_3.wav for seamless loop - delete old dungeon_ambiencess

--- main.gd
+++ main.gd
@@ -8,4 +8,4 @@ func _enter_tree() -> void:
 	Global.main = self
 
 func _ready() -> void:
-	looping_ambience_sfx_id = Audio.play_sfx("dungeon_ambience.mp3")
+	looping_ambience_sfx_id = Audio.play_sfx("dungeon_ambience_3.wav")
