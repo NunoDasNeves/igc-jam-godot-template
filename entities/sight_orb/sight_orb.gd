91094f8c719b68cde360cc3199138aa137b1d739
Nuno Das Neves <ndasneves@gmail.com>
2025-04-27 10:11:52 -0700

    add sight orb animation

--- entities/sight_orb/sight_orb.gd
+++ entities/sight_orb/sight_orb.gd
@@ -1,5 +1,10 @@
 class_name SightOrb extends Entity
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	animated_sprite_2d.play("default")
+
 func collect():
 	Events.entity_collected.emit(self)
 	# can't free here because the collectible may be used later
