d805640348df1ba36af9436161dc08a0e7025c28
Nuno Das Neves <ndasneves@gmail.com>
2025-04-27 17:02:30 -0700

    play music with an AudioStreamInteractive to control the different clips

--- main.gd
+++ main.gd
@@ -2,6 +2,7 @@ class_name Main extends Node
 
 @onready var pause_menu = $UI/PauseMenu
 @onready var level_completed_menu: PanelContainer = $UI/LevelCompletedMenu
+@onready var music_player: AudioStreamPlayer2D = $MusicPlayer
 
 var looping_ambience_sfx_id: int
 
@@ -10,3 +11,8 @@ func _enter_tree() -> void:
 
 func _ready() -> void:
 	looping_ambience_sfx_id = Audio.play_sfx("dungeon_ambience_3.wav")
+	#update_music_order()
+
+func update_music_order():
+	var stream: AudioStreamInteractive = music_player.stream
+	stream.set_clip_auto_advance_next_clip(1,1)
