f2b193ffa1e3e6c0a616e4611b113dc40a97de6f
Nuno Das Neves <ndasneves@gmail.com>
2025-04-29 22:13:04 -0700

    fix mouse pointer pos on stretched viewport - disable stretching

--- ui/options.gd
+++ ui/options.gd
@@ -1,9 +1,6 @@
-extends MarginContainer
+extends PanelContainer
 
-@onready var back_button: Button = $VBoxContainer/BackButton
+@onready var back_button: Button = $MarginContainer/VBoxContainer/BackButton
 
 func _ready() -> void:
 	back_button.connect("button_down", func(): Events.options_closed.emit())
-	#if Global.is_web:
-	#	sfx_label.hide()
-	#	sfx_slider.hide()
